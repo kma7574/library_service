14d1551dc731bf3e740c5bad954ead37162b8414
DESKTOP-98LVKQH\kma75 <kma7574@naver.com>
2021-11-20 04:35:27 +0900

    대여현황, 잔여수량 테이블 추가

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import relationship
 from db_connect import db
 from datetime import datetime
 
@@ -36,3 +37,37 @@ class Member(db.Model):
         self.gender = gender
         self.phone_number = phone_number
         self.address = address
+
+
+#대여 현황 테이블
+class Book_borrow(db.Model):
+    __tablename__ = 'book_borrow'
+
+    id = db.Column(db.Integer, primary_key=True,
+                   nullable=False, autoincrement=True)
+    borrow_book_id = db.Column(
+        db.Integer, db.ForeignKey(Book.id), nullable=False)
+    borrow_user_id = db.Column(
+        db.Integer, db.ForeignKey(Member.id), nullable=False)
+    borrow_date = db.Column(db.TIMESTAMP, server_default=db.func.now(),
+                            onupdate=db.func.now())
+    borrow_state = db.Column(db.TINYINT, nullable=False,
+                             default=0)  # 대출현황(반납x: 0, 반납o: 1)
+
+    def __init__(self, borrow_book_id, borrow_user_id):
+        self.borrow_book_id = borrow_book_id
+        self.borrow_user_id = borrow_user_id
+
+
+#대여 후 남은 책, 수량 등 현황 테이블
+class Book_remain(db.Model):
+	__tablename__ = "book_remain"
+	
+	id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+	remain_book_id = db.Column(db.Integer, db.ForeignKey(Book.id), nullable=False)
+	remain_book_count = db.Column(db.Integer, nullable=False, default=5)
+
+	def __init__(self, remain_book_id, remain_book_count):
+		self.remain_book_id = remain_book_id
+		self.remain_book_count = remain_book_count
+		
