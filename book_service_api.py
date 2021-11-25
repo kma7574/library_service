28443928f5fff9222522cb8b5921aa752288f6cc
DESKTOP-98LVKQH\kma75 <kma7574@naver.com>
2021-11-25 14:44:16 +0900

    평점이 출력되지 않아서 추가해줌

--- book_service_api.py
+++ book_service_api.py
@@ -13,13 +13,13 @@ def show_list():
 
     if request.method == 'POST':
         search_word = request.form['searchWord']
-        book_list = db.session.query(Book.id, Book.book_name, Book.path, Book_remain.remain_book_count).join(Book, Book_remain.id == Book.id).filter(Book.book_name.like(f'%{search_word}%'))
+        book_list = db.session.query(Book.id, Book.book_name, Book.path, Book.rating, Book_remain.remain_book_count).join(Book, Book_remain.id == Book.id).filter(Book.book_name.like(f'%{search_word}%'))
         search_count = book_list.count()
         page = request.args.get('page', type=int, default=1)  # 페이지
         pagination = book_list.paginate(page, per_page=8)
         return render_template('inventory.html', book_list = book_list, search_count = search_count, search_word=search_word, pagination=pagination)
     else:
-        book_list = book_list = db.session.query(Book.id, Book.book_name, Book.path, Book_remain.remain_book_count).join(Book, Book_remain.id == Book.id)
+        book_list = book_list = db.session.query(Book.id, Book.book_name, Book.path, Book.rating, Book_remain.remain_book_count).join(Book, Book_remain.id == Book.id)
         search_count = book_list.count()
         page = request.args.get('page', type=int, default=1)  # 페이지
         pagination = book_list.paginate(page, per_page=8)
